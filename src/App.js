5fa174c457c5c96365b88d7c9d92279ed639ad92
Matt <mattbird1406@gmail.com>
2020-10-03 16:30:04 +1000

    modal

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React from "react"; //imports the default export
+import React, { useState } from "react"; //imports the default export
 import { render } from "react-dom";
 import { Link, Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const themeHook = useState("peru");
   return (
-    <div>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Router>
-        <SearchParams path="/" />
-        <Details path="/details/:id" />
-      </Router>
-    </div>
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
   );
 };
 
--- src/Pet.js
+++ src/Pet.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const Pet = ({ name, animal, breed, media, location, id }) => {
   let hero = "http://placecorgi.com/300/300";
-  if(media.length){
+  if (media.length) {
     hero = media[0].small;
   }
 
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -15,8 +16,8 @@ const Pet = ({ name, animal, breed, media, location, id }) => {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
-  )
+    </Link>
+  );
 };
 
 export default Pet;
